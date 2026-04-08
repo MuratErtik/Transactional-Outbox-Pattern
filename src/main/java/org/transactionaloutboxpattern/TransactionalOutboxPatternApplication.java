56ff6c54fbac098b4357aa41666d58e86715f5e9
Murat Ertik <dmuratertik1@gmail.com>
2026-04-08 13:29:01 +0300

    first commit

---
+++ src/main/java/org/transactionaloutboxpattern/TransactionalOutboxPatternApplication.java
@@ -0,0 +1,13 @@
+package org.transactionaloutboxpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalOutboxPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalOutboxPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/transactionaloutboxpattern/TransactionalOutboxPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package org.transactionaloutboxpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalOutboxPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
