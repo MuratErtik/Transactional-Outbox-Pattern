075252e0008be3c5e9d0c6b00729faff479a04f8
Murat Ertik <dmuratertik1@gmail.com>
2026-04-09 14:25:27 +0300

    order entity created.

---
+++ Order/src/main/java/org/order/Order.java
@@ -0,0 +1,38 @@
+package org.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long customerId;
+
+    private Long productId;
+
+    private BigDecimal price;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+
+}
