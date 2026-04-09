ba7820b768355aa4ab7aae2e07325d7ccde935e1
Murat Ertik <dmuratertik1@gmail.com>
2026-04-09 14:19:22 +0300

    payment entity created.

---
+++ Payment/src/main/java/org/payment/Payment.java
@@ -0,0 +1,36 @@
+package org.payment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;
+
+    private BigDecimal amount;
+
+    private String source;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
