8c8b33b362376cb284eb128e17048e5ee93b8c15
Murat Ertik <dmuratertik1@gmail.com>
2026-04-10 11:55:08 +0300

    findByOrderId method added.

--- Payment/src/main/java/org/payment/PaymentRepository.java
+++ Payment/src/main/java/org/payment/PaymentRepository.java
@@ -3,7 +3,9 @@ package org.payment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "payments", path = "payments")
 public interface PaymentRepository  extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(Long orderId);
 }
